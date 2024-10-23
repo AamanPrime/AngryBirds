29e508a81100200e3405748a40b819de02cd1de8
Akshat-Sin <akshat23064@iiitd.ac.in>
2024-10-23 16:53:38 +0530

    updating Exception classes

---
+++ core/src/main/java/com/sampleproject/Exception/unableToRestartGameException.java
@@ -0,0 +1,9 @@
+package com.sampleproject.Exception;
+
+public class unableToRestartGameException extends Exception {
+
+    public unableToRestartGameException(String message) {
+        super(message);
+    }
+
+}
